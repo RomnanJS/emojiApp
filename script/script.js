57ae5f1680fa97c3337901eafbaaea30f1930859
RomanJS <pozdnakovroman46@gmail.com>
2022-04-23 18:50:21 +0300

    Change header

---
+++ script/script.js
@@ -0,0 +1,32 @@
+// let main = document.getElementsByTagName("main");
+// let new_element = document.createElement("div"); ///main[0].append(document.createElement("p"))
+// // main_element.innerText = "Так добавить текс внутри тега p";
+// main[0].append(new_element);
+// new_element.innerHTML = "<img src = 'img/img.png  ' >  ";
+
+// new_element.append((document.createElement("p").innerText = "100"));
+// new_element.append(
+//   (document.createElement("p").innerText =
+//     "Hundred, points, symbol, wow, win, perfect, parties")
+// );
+// new_element.innerText;
+// text-decoration: underline;
+// let new1 = document.createElement("p");
+// new_element.append(new1);
+// new1.innerText = "100";
+// let new2 = document.createElement("p");
+// new_element.append(new2);
+// new2.innerText = "Hundred, points, symbol, wow, win, perfect, parties";
+
+// let main = document.getElementsByTagName("main")[0];
+// let param = " Я переменная";
+// main.insertAdjacentHTML("afterbegin", "<p>" + param + "</p>");
+
+// main.insertAdjacentElement("afterbegin", document.createElement("section"));
+// /КЛОНИРУЕМ ОБЬЕКТ/; //
+// let clone = main.cloneNode(false);
+// console.log(clone);
+// let section = document.getElementsByTagName("section")[0];
+// section.insertAdjacentHTML("afterbegin", "<div> привет </div>");
+
+document;
